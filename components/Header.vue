122fe10c0c35f485fcd8e1ea4e4b3daccda36262
sckoorp <ckamalennour@gmail.com>
2024-06-19 21:31:48 +0100

    update: updated navbar

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,5 @@
 <template>
-    <div class="flex justify-between items-center p-4 select-none">
+    <div class="flex justify-center items-center p-4 select-none">
         <NuxtLink to="/" class="text-light text-3xl font-black rounded-sm uppercase">AniBase</NuxtLink>
-        <NuxtLink class="text-dark bg-prime text-lg font-semibold rounded-sm px-2">Login</NuxtLink>
     </div>
 </template>
-
-<script lang="ts" setup>
-</script>
